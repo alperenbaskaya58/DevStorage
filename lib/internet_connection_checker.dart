afa992900f598030f64c0f57424bd4fc234b1c4b
alperenbaskaya58 <alperenbaskaya0@gmail.com>
2022-07-12 16:24:08 +0300

    dep

---
+++ lib/internet_connection_checker.dart
@@ -0,0 +1,84 @@
+library dev_storage_package;
+
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+
+class InternetConnectionCheckerDev extends GetxController{
+
+Function() disconnecetFunction;
+Function()? connectedBackFunction;
+
+
+InternetConnectionCheckerDev (
+  this.disconnecetFunction,
+  this.connectedBackFunction 
+  );
+
+late StreamSubscription<InternetConnectionStatus> _connectivitySubscription;
+ 
+late bool isConnected;
+
+int flag = 0;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+      
+      _connectivitySubscription =  InternetConnectionChecker().onStatusChange.listen((InternetConnectionStatus result) async {
+
+         log('\x1B[32m${result.toString()}\x1B[0m');
+
+         switch (result)
+         {
+          case InternetConnectionStatus.connected : 
+          {
+          try {
+          final result = await InternetAddress.lookup('example.com');
+          if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+
+           changeConnectionStatus(true);
+            update();
+          }
+        } on SocketException catch (_) {
+         changeConnectionStatus(false);
+           log('\x1B[32m Not Connected \x1B[0m');
+           disconnecetFunction();
+        }
+
+        break;
+          }
+
+        case InternetConnectionStatus.disconnected : {
+          changeConnectionStatus(false);
+          log('\x1B[32m Not Connected \x1B[0m');
+          disconnecetFunction();
+          break;
+        }   
+         }
+    
+  });
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    _connectivitySubscription.cancel();
+  }
+
+  changeConnectionStatus (boolX){
+    if(boolX == true && flag > 0){
+      if(connectedBackFunction != null){
+        connectedBackFunction!();
+      }
+    }
+    
+    isConnected = boolX;
+    flag++;
+  }
+
+}
